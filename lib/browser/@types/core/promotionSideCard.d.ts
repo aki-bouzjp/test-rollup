4f24b5fa24b2da8b819a424e53874d690578c7d0
takaaki-mapbox <takaaki.kuwabara@mapbox.com>
2021-10-15 23:14:39 +0900

    fix

--- lib/browser/@types/core/promotionSideCard.d.ts
+++
@@ -1,9 +0,0 @@
-declare class PromotionSideCard {
-    private _promoted?;
-    get id(): string;
-    initPromoted(promoted: MapboxPromoted): void;
-    private sendAction;
-    show(feature: Feature): void;
-    private remove;
-}
-export default PromotionSideCard;
--- lib/browser/@types/utils/urls.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const getImageUrl: (imageId: string) => string;
